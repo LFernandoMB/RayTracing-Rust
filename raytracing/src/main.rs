8c401aee60efb3cc45cbe900d247e6f6e1e02555
Luis Fernando M Bezerra <l.fernandomb7@gmail.com>
2023-07-24 07:52:58 +0100

    feat Update main
    Inserting my main

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -10,26 +10,26 @@
 // Transposing challenge code from c++ to Rust
 
 fn main() {
-    // Image
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    // Creating the image dimensions - width = 256 and height = 256
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
 
-    // Render
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("255");
+    // Creating the Render
+    print!("P3");
+    print!("{} {}",image_width, image_height);
+    print!("255");
 
-    for j in (0..IMAGE_HEIGHT).rev() {
-        for i in 0..IMAGE_WIDTH {
-            let r = f64::from(i) / f64::from(IMAGE_WIDTH - 1);
-            let g = f64::from(j) / f64::from(IMAGE_HEIGHT - 1);
+    for i in (0..image_height).rev() {
+        for j in (0..image_width) {
+            let r = f64::from(i) / f64::from(image_width-1);
+            let g = f64::from(j) / f64::from(image_height-1);
             let b = 0.25;
 
             let ir = (255.999 * r) as i32;
             let ig = (255.999 * g) as i32;
             let ib = (255.999 * b) as i32;
 
-            println!("{} {} {}", ir, ig, ib);
+            println!("{ir} {ig} {ib}");
         }
     }
 }
